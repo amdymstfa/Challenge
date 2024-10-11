e1ab63cce99a5ae3795c15d323c6dcd5d08c0086
amdymstfa <amdymoustapha011@gmail.com>
2024-10-11 15:55:27 +0100

    Exercice 2 : Manipulation de variables numériques

---
+++ challenge01/variables_manage.js
@@ -0,0 +1,6 @@
+let a = 2 , b = 8;
+
+console.log("the sum is", a+b);
+console.log("the difference is", a-b);
+console.log("the product is", a*b);
+console.log("the remain of the division is", a%b);
